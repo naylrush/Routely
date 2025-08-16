e22f49dc00fa767259078a5cfa37ac55b12a5d51
Alexey Davletshin <naylrush@gmail.com>
2025-08-16 23:42:53 +0300

    Remove

--- Sources/Ro/Sources/Routing/Views/FullScreen/Extensions/EnvironmentValues+IsDragging.swift
+++
@@ -1,9 +0,0 @@
-//
-// Copyright © 2025 Движ
-//
-
-import SwiftUI
-
-extension EnvironmentValues {
-    @Entry public var isDragging = false
-}
--- Sources/RoutelyInterfaces/Models/DebugInfo.swift
+++
@@ -1,9 +0,0 @@
-#if DEBUG
-import Foundation
-
-public enum DebugInfo {
-    public static var isInPreview: Bool {
-        ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
-    }
-}
-#endif
