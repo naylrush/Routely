9e337014ee05c0300cd59ad64b3bd1ab4db81210
Alexey Davletshin <naylrush@gmail.com>
2025-11-03 00:09:00 +0300

    Support of universal links

--- Sources/Routely/DeepLinking/ViewExtensions/View+onOpenDeepLink.swift
+++ Sources/Routely/DeepLinking/ViewExtensions/View+onOpenDeepLink.swift
@@ -6,20 +6,46 @@ import OSLog
 import SwiftUI
 
 extension View {
+    public typealias RawDeepLinkHandler = @Sendable (RawDeepLink) async -> Void
+    private typealias URLHandler = (URL) -> Void
+
     public func onOpenDeepLink(
-        perform: @Sendable @escaping (RawDeepLink) async -> Void,
+        perform: @escaping RawDeepLinkHandler,
         `if` condition: Bool
     ) -> some View {
-        onOpenURL { url in
-            guard condition else { return }
+        let handler: URLHandler = { url in
+            Self.handleUrl(url: url, perform: perform, condition: condition)
+        }
 
-            guard let deepLink = LinkParser.parseDeepLink(url: url) else {
-                logger.error("Failed to parse deeplink: \(url)")
-                return
+        return self
+            .onOpenURL(perform: handler)
+            .onOpenUniversalLink(perform: handler)
+    }
+
+    private func onOpenUniversalLink(
+        perform: @escaping URLHandler,
+    ) -> some View {
+        // Handles: Camera QR codes, Spotlight, Handoff
+        onContinueUserActivity(NSUserActivityTypeBrowsingWeb) { userActivity in
+            if let url = userActivity.webpageURL {
+                perform(url)
             }
-            Task {
-                await perform(deepLink)
-            }
+        }
+    }
+
+    private static func handleUrl(
+        url: URL,
+        perform: @escaping RawDeepLinkHandler,
+        condition: Bool
+    ) {
+        guard condition else { return }
+
+        guard let deepLink = LinkParser.parseDeepLink(url: url) else {
+            logger.error("Failed to parse deeplink: \(url)")
+            return
+        }
+        Task {
+            await perform(deepLink)
         }
     }
 }
